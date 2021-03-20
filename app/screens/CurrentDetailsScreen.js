6af117dbc82af04597b7dd6b9fd3d11dfc429e29
Rakib Ali <r.ali@se18.qmul.ac.uk>
2021-03-20 00:27:30 +0000

    live update add books

--- app/screens/CurrentDetailsScreen.js
+++ app/screens/CurrentDetailsScreen.js
@@ -11,27 +11,70 @@ import Card from "../components/Card";
 import CardDeleteAction from "../components/CardDeleteAction";
 
 function CurrentDetailsScreen({navigation}) {
-  const[state,setState]= useState({
-    results: [],
-    
-  });
-  useEffect(() =>{
-    let Result=[];
-    firebase.firestore().collection("users")
-    .doc(firebase.auth().currentUser.uid).collection("currently reading").onSnapshot((snapshot)=>{
-      snapshot.docs.forEach(doc =>{
-        Result.push(doc.data())
-      })
-      setState({results: Result});
+  const[load,setLoad]=useState(true);
+  const[current,setCurrent]= useState([]);
+  // function addedBook(doc){
+  //   console.log(doc.data())
+  // }
+  // useEffect(() =>{
+  //   let Result=[];
+  //   firebase.firestore().collection("users")
+  //   .doc(firebase.auth().currentUser.uid).collection("currently reading").get().then((snapshot)=>{
+  //     snapshot.docs.forEach(doc =>{
+  //       Result.push(doc.data())
+  //     })
+  //     setState(Result);
+  //     setLoad(!load)
+  //   })
+  // },[])
+  useEffect(()=>{
+    //can call a separate function to get all books
+    const subscriber=firebase.firestore().collection("users")
+    .doc(firebase.auth().currentUser.uid).collection("currently reading").onSnapshot(snapshot=>{
+      const change=snapshot.docChanges()
+      change.forEach((change)=>{
+        console.log(change.doc.data())//This shows every chnage thats happening
+        console.log(change.type)
+        if(change.type==="added"){//checks if any books were added into the db
+          let updateAdd =[]
+          firebase.firestore().collection("users")
+          .doc(firebase.auth().currentUser.uid).collection("currently reading").get().then((snapshot)=>{
+            snapshot.forEach((doc)=>{
+              updateAdd.push(doc.data())
+            })
+             setCurrent(updateAdd)
+          })
+        }
+        // if(change.type==="removed"){
+        //   subscriber()
+        //   console.log("removed")//If doc was removed this will log
+        //   let update =[]
+        //   firebase.firestore().collection("users")
+        //   .doc(firebase.auth().currentUser.uid).collection("currently reading").get().then((snapshot)=>{
+        //     snapshot.forEach((doc)=>{
+        //       update.push(doc.data())
+        //     })
+        //      setCurrent(update)
+        //   })
+        // }
+      }
+      )
     })
+    
   },[])
   useEffect(()=>{
-    
+
   },[])
   const handleDelete= item =>{
     firebase.firestore().collection("users")
     .doc(firebase.auth().currentUser.uid).collection("currently reading").doc(item.title).delete()
     .then(()=>{
+      const subscriber=firebase.firestore().collection("users")
+    .doc(firebase.auth().currentUser.uid).collection("currently reading").onSnapshot(snapshot=>{
+      const change=snapshot.docChanges()
+
+
+    })
       Alert.alert('Removed from library')
     }).catch((e)=>{
       Alert.alert('Error:' + e)
@@ -41,7 +84,7 @@ function CurrentDetailsScreen({navigation}) {
     return (
       <Screen > 
         <FlatList
-        data={state.results}
+        data={current}
         renderItem={({item}) => (
           <Card
             title={item.title}
