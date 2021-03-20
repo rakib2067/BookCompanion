666209bc1154a1a316c98839c5ab69aa343c722c
Rakib Ali <r.ali@se18.qmul.ac.uk>
2021-03-20 23:04:58 +0000

    Update CurrentDetailsScreen.js

--- app/screens/CurrentDetailsScreen.js
+++ app/screens/CurrentDetailsScreen.js
@@ -13,7 +13,28 @@ import CardDeleteAction from "../components/CardDeleteAction";
 function CurrentDetailsScreen({navigation}) {
   const[deleted,setDeleted]=useState(true)
   const[current,setCurrent]= useState([]);
-
+  const[refresh,setRefresh]=useState(true);
+  const [name,setName]=useState({
+      exp:null,
+      level:null,
+      target:null
+    })
+    useEffect(()=>{
+      firebase.firestore().collection("points")
+      .doc(firebase.auth().currentUser.uid).get().then((doc)=>{
+        const Ref=doc.data();
+        setName({
+          exp:Ref.exp,
+          level:Ref.level,
+          target:Ref.target
+        })
+      })
+    },[])
+    useEffect(()=>{
+      if(name.level==1&&name.exp==70){
+        Alert.alert("Delete the book from your library")
+      }
+  },[name])
   useEffect(()=>{
     const subscriber=firebase.firestore().collection("users")
     .doc(firebase.auth().currentUser.uid).collection("currently reading").onSnapshot(snapshot=>{
@@ -62,7 +83,16 @@ function CurrentDetailsScreen({navigation}) {
     .doc(firebase.auth().currentUser.uid).collection("currently reading").doc(item.title).delete()
     .then(()=>{
       setDeleted(!deleted)
-      Alert.alert('Removed from library')
+      if(name.level==1 && name.exp==70){
+        firebase.firestore().collection("points")
+                .doc(firebase.auth().currentUser.uid).set({
+                  exp:name.exp+10
+                },{merge:true}).then(setRefresh(!refresh))
+                Alert.alert("Congratulations, You just earned 10xp!")
+        }
+        else{
+          Alert.alert('Removed from library')
+        }
     }).catch((e)=>{
       Alert.alert('Error:' + e)
     })
