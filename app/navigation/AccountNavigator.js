e65398441453604f3a0e0b83dd529def5d8b13ab
Rakib Ali <r.ali@se18.qmul.ac.uk>
2021-03-25 23:48:04 +0000

    changes

--- app/navigation/AccountNavigator.js
+++ app/navigation/AccountNavigator.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import AccountScreen from "../screens/AccountScreen";
-import MessagesScreen from "../screens/MessagesScreen";
 import WelcomeScreen from "../screens/WelcomeScreen";
 
 
@@ -10,7 +9,6 @@ const Stack= createStackNavigator();
 const AccountNavigator= () =>(
     <Stack.Navigator>
         <Stack.Screen name="Account" component={AccountScreen}/>
-        <Stack.Screen name="Messages" component={MessagesScreen} />
         <Stack.Screen name="Welcome" component={WelcomeScreen} options={{headerShown:false}} />
     </Stack.Navigator>
 )
